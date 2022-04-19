f6decac14911d615b91dd03450c9c1dbe16c0efc
laptop <thangbin1997@gmail.com>
2022-04-19 20:22:19 +0700

    laptop

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,4 +1,4 @@
-import {React,useEffect} from 'react'
+import {React,useEffect, useRef} from 'react'
 import { FiLink } from 'react-icons/fi';
 import { RiShareForwardLine } from 'react-icons/ri';
 import { BsThreeDots } from 'react-icons/bs';
@@ -13,19 +13,22 @@ function Profile() {
     const profileId = useLocation();
     var profileById=datasVideos.find(getProfile=>getProfile.id == profileId.state.id)
 
+    const ibrpBox= useRef()
+    var timeOut;
     return(
         <div className='container__profile' key={profileById.id}>
             <div className="profile__info">
+                <div className="profile__info__left">
                 <div className="profile__info--head">
                     <div className="profile__info--head--avatar">
                         <img src={profileById.avatar} alt="avatar" />
                     </div>
                     <div className="profile__info--head--textinfo">
                         <div className="profile__info--head--textinfo--nickname">
-                            <h1>{profileById.nickname}</h1>
+                            <h1>{profileById.names}</h1>
                         </div>
                         <div className="profile__info--head--textinfo--name">
-                            <h3>{profileById.names}</h3>
+                            <h3>{profileById.nickname}</h3>
                         </div>
                         <div className="profile__info--head--textinfo--btn">
                             <button className='btn--bgred--textwhile'>Follow</button>
@@ -35,15 +38,15 @@ function Profile() {
                 <div className="profile__info--body">
                     <div className="profile__info--body--count">
                         <div className="profile__info--body--count--follow">
-                            <p>{profileById.share}</p>
+                            <p><strong>{profileById.share}</strong></p>
                             <span>Đang Follow</span>
                         </div>
                         <div className="profile__info--body--count--follower">
-                            <p>{profileById.comments}</p>
+                            <p><strong>{profileById.comments}</strong></p>
                             <span>Follower</span>
                         </div>
                         <div className="profile__info--body--count--like">
-                            <p>{profileById.like}</p>
+                            <p><strong>{profileById.like}</strong></p>
                             <span>Thích</span>
                         </div>
                     </div>
@@ -55,22 +58,32 @@ function Profile() {
                         <span>biolink.com.vn/tiktoklive</span>
                     </div>
                 </div>
-            </div>
-            <div className="profile__info__right">
-                <div className="profile__info__right--share">
-                    <RiShareForwardLine className='icon share__btn'/>
-                    <Linktoshare/>
                 </div>
-                <div className="profile__info__right--ibrp">
-                    <BsThreeDots className='icon there__dot'/>
-                    <div className="there__dot--box">
-                        <div className="there__dot--box--ib">
-                            <FaRegPaperPlane className="there__dot--icon"/>
-                            <span>Gửi tin nhắn</span>
-                        </div>
-                        <div className="there__dot--box--report">
-                            <FiFlag className="there__dot--icon"/>
-                            <span>Báo cáo</span>
+                <div className="profile__info__right">
+                    <div className="profile__info__right--share" >
+                        <RiShareForwardLine className='icon share__btn'/>
+                        <Linktoshare className="profile__info__right--share--box"/>
+                    </div>
+                    <div className="profile__info__right--ibrp">
+                        <BsThreeDots className='icon there__dot' 
+                            onMouseEnter={()=>{ibrpBox.current.style.display="block"}}
+                            onMouseLeave={()=>{timeOut=setTimeout(() => {
+                                ibrpBox.current.style.display="none"
+                            }, 300);}}
+                        />
+                        <div className="there__dot--box" ref={ibrpBox} onMouseEnter={()=>{ibrpBox.current.style.display="block";clearTimeout(timeOut)}}
+                            onMouseLeave={()=>{timeOut=setTimeout(() => {
+                                ibrpBox.current.style.display="none"
+                            }, 300);}}
+                            >
+                            <div className="there__dot--box--ib">
+                                <FaRegPaperPlane className="there__dot--icon"/>
+                                <span>Gửi tin nhắn</span>
+                            </div>
+                            <div className="there__dot--box--report">
+                                <FiFlag className="there__dot--icon"/>
+                                <span>Báo cáo</span>
+                            </div>
                         </div>
                     </div>
                 </div>
