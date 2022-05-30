2586a5e6f32aa113c0fe052ffe6570b95bf4dfb0
thangbin1997 <76263303+thangbin1997@users.noreply.github.com>
2022-05-30 21:58:12 +0700

    end second time

--- src/pages/Message.jsx
+++ src/pages/Message.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import messageStyle from '../access/style/message.scss'
+import { AiOutlineArrowLeft } from 'react-icons/ai';
+import { BsFillGearFill } from 'react-icons/bs';
+
 
 function Message() {
   return (
-    <div>Message</div>
+    <div className='message__container'>
+      <div className="message__content">
+        <div className="message__content__back-container">
+          <AiOutlineArrowLeft  className='arrow-left-icon'/>
+        </div>
+        <div className="message__content__left">
+          <div className="message__content__left-header">
+            <h2>Tin nhắn</h2>
+            <BsFillGearFill  className='wheel-icon'/>
+          </div>
+          <div className="message__content__left-content">
+            <div className="message__content__left-content-item">
+              <span>Chưa có tín nhắn nào</span>
+            </div>
+          </div>
+        </div>
+        <div className="message__content__right">
+
+        </div>
+      </div>
+    </div>
   )
 }
 
